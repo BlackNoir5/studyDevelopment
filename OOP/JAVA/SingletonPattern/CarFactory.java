d50f913ec52df474c00fdd025c44ffb4c9c29444
Fillip <103233073+BlackNoir5@users.noreply.github.com>
2024-10-09 17:51:05 +0900

    Create CarFactory.java

---
+++ OOP/JAVA/SingletonPattern/CarFactory.java
@@ -0,0 +1,21 @@
+public class CarFactory {
+   
+    private CarFactory(){};
+
+    private static CarFactory instance = new CarFactory();
+
+    public static CarFactory getInstance() {
+            
+        if( instance == null) {
+            instance = new CarFactory();
+        }
+        return instance;
+            
+    }
+
+    public Car createCar(){
+        Car car = new Car();
+        return car;
+    }
+    
+}
